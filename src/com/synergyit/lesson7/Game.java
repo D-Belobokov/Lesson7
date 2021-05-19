10726d65b164cc37a0d491a457964a22261bb647
voyag <voyger0781@gmail.com>
2021-05-19 19:22:27 +0300

    дора,отали логику программы

--- src/com/synergyit/lesson7/Game.java
+++ src/com/synergyit/lesson7/Game.java
@@ -3,7 +3,7 @@ import java.util.Random;
 
 public class Game {
     public static void main(String[] args) {
-        Random random=new Random();
+        Random random = new Random();
         Block[] blocks = new Block[10];
         for (int i = 0; i < blocks.length; i++){
             int j = random.nextInt(2);
@@ -13,19 +13,11 @@ public class Game {
                 blocks[i] = new RunningWay();
             }
         }
-        User[] users = new User[5];
-        for (int i = 0; i < users.length; i++) {
-            int j = random.nextInt(3);
-            if (j == 0) {
-                users[i] = new Kiborg();
-            }
-            if (j == 1) {
-                users[i] = new Human();
-            }
-            if (j == 2) {
-                users[i] = new Cat();
-            }
-        }
+        User[] users = new User[3];
+
+        users[0] = new Kiborg();
+        users[1] = new Human();
+        users[2] = new Cat();
 
         for (int i = 0; i < users.length; i++) {
             for (int j = 0; j < blocks.length; j++) {
@@ -34,7 +26,7 @@ public class Game {
                         users[i].jump();
                     } else {
                         System.out.println("Участник " + users[i].getName() + " сошел с дистанции");
-                        System.out.println("арьер");
+                        System.out.println("Препятствие - Барьер");
                         System.out.println(users[i].getjMax() + " " + blocks[j].getBlock());
                         break;
                     }
@@ -43,25 +35,12 @@ public class Game {
                         users[i].run();
                     } else {
                         System.out.println("Участник " + users[i].getName() + " сошел с дистанции");
-                        System.out.println(",ег");
+                        System.out.println("Препятствие - Бег");
                         System.out.println(users[i].getrMax() + " " + blocks[j].getBlock());
                         break;
                     }
                 }
             }
         }
-
-        /*for(int i=0;i<array.length;i++){
-            if(array[i]<=f.getjMax()) {
-                System.out.println("Дистанция m:"+array[i]+"\tКиборг бежит");
-            }
-            else if(i==10){
-                System.out.println("Дистанция пройдена");
-            }
-            else{
-                System.out.println("киборг отбегался");
-                break;
-            }
-        }*/
     }
 }
